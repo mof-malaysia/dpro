2bd4396fcc5ab0f6fbb0954c6b09c1e87af9fcd5
Len E-Herng <leneherng@gmail.com>
2025-08-11 14:53:40 +0800

    feat: format date in ISO format

--- src/utilities/formatDateTime.ts
+++ src/utilities/formatDateTime.ts
@@ -10,3 +10,11 @@ export const formatDate = (timestamp: string): string =>
     month: 'long',
     day: 'numeric',
   })}`
+
+export const formatISODate = (d: Date) => {
+  return [
+    d.getFullYear(),
+    ('0' + (d.getMonth() + 1)).slice(-2),
+    ('0' + d.getDate()).slice(-2),
+  ].join('-')
+}
