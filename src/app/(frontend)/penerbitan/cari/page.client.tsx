0f12ab18660fbef7853cd14b6b0e715edfa2224c
Len E-Herng <leneherng@gmail.com>
2025-08-11 15:18:48 +0800

    feat: use `cari` param for search results

---
+++ src/app/(frontend)/penerbitan/cari/page.client.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { DateRangePicker } from '@/components/DateRangePicker'
+import { SearchBar } from '@/components/SearchBar'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const PageClient: React.FC = () => {
+  const pathname = usePathname()
+
+  return (
+    <>
+      <div className="xl:col-span-6">
+        <SearchBar />
+      </div>
+
+      <DateRangePicker pathname={pathname} />
+    </>
+  )
+}
+
+export default PageClient
