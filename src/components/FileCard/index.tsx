85b5ed46d0b8dc278bb8fdc8a2d6bf49dd58b796
Len E-Herng <leneherng@gmail.com>
2025-08-20 09:36:00 +0800

    refactor: new publication card design

--- src/components/FileCard/index.tsx
+++ src/components/FileCard/index.tsx
@@ -1,14 +1,35 @@
 'use client'
+
+import { Media } from '@/components/Media'
+import { Button } from '@/components/ui/button'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
+import { useIsMobile } from '@/hooks/useIsMobile'
 import type { Penerbitan } from '@/payload-types'
 import { cn } from '@/utilities/ui'
-import useClickableCard from '@/utilities/useClickableCard'
-import { ArrowOutgoingIcon } from '@govtechmy/myds-react/icon'
-import Image from 'next/image'
+import {
+  Dialog,
+  DialogBody,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@govtechmy/myds-react/dialog'
+import { ArrowOutgoingIcon, CrossIcon, DownloadIcon } from '@govtechmy/myds-react/icon'
 import Link from 'next/link'
 import React from 'react'
-import { Button } from '../ui/button'
 
-export type CardFileData = Pick<Penerbitan, 'name' | 'fileUpload'>
+export type CardFileData = Pick<Penerbitan, 'description' | 'image' | 'name' | 'fileUpload'>
 
 export const FileCard: React.FC<{
   alignItems?: 'center'
@@ -16,61 +37,145 @@ export const FileCard: React.FC<{
   doc?: CardFileData
   title?: string
 }> = (props) => {
-  const { card, link } = useClickableCard({ external: true, newTab: true })
   const { className, doc } = props
-  const { name, fileUpload } = doc || {}
+  const { description, image, name, fileUpload } = doc || {}
+
+  const isMobile = useIsMobile()
+
+  if (isMobile)
+    return (
+      <Drawer>
+        <DrawerTrigger asChild>
+          <article
+            className={cn(
+              'group flex flex-col gap-6 p-4.5 border rounded-lg overflow-hidden',
+              'hover:cursor-pointer hover:border-otl-primary-300 hover:ring ring-fr-primary',
+              className,
+            )}
+          >
+            <div className="relative w-full grow space-y-4">
+              {image && typeof image !== 'string' && (
+                <Media
+                  htmlElement={null}
+                  resource={image}
+                  imgClassName="border rounded-md object-cover h-[200px]"
+                />
+              )}
+              <div className="space-y-3">
+                {name && (
+                  <p className="font-semibold line-clamp-2 group-hover:text-txt-primary">{name}</p>
+                )}
+                {description && (
+                  <p className="text-sm text-txt-black-500 line-clamp-2">{description}</p>
+                )}
+              </div>
+            </div>
+            <Button
+              variant="primary-outline"
+              className="group-hover:bg-bg-primary-600 group-hover:border group-hover:border-otl-primary-600 group-hover:text-white"
+            >
+              Info Lanjut
+              <ArrowOutgoingIcon />
+            </Button>
+          </article>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            {name && <DrawerTitle>{name}</DrawerTitle>}
+            <DrawerClose>
+              <Button variant="default-outline" iconOnly>
+                <CrossIcon />
+                <span className="sr-only">Close</span>
+              </Button>
+            </DrawerClose>
+          </DrawerHeader>
+          <div className="p-4.5 flex flex-1 flex-col gap-4.5 overflow-y-auto">
+            {image && typeof image !== 'string' && (
+              <Media
+                htmlElement={null}
+                resource={image}
+                imgClassName="border rounded-md object-cover h-[250px]"
+              />
+            )}
+            {description && <DialogDescription>{description}</DialogDescription>}
+          </div>
+          <DrawerFooter className="*:w-full">
+            <DrawerClose asChild>
+              {fileUpload && typeof fileUpload !== 'string' && fileUpload.url && (
+                <Link href={fileUpload.url} target="_blank">
+                  <Button size="md" variant="primary-fill" className="w-full justify-center">
+                    <DownloadIcon />
+                    Muat Turun Dokumen
+                  </Button>
+                </Link>
+              )}
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    )
 
   return (
-    <article
-      className={cn(
-        'group relative border max-h-[300px] w-full rounded-lg overflow-hidden select-none',
-        'hover:cursor-pointer hover:border-otl-primary-300 hover:ring ring-fr-primary',
-        className,
-      )}
-      ref={card.ref}
-    >
-      {fileUpload ? (
-        <>
-          <Image
-            className="object-fill"
-            src="/penerbitan-bg.png"
-            width={338}
-            height={300}
-            alt="Background"
-            style={{
-              width: '100%',
-            }}
-          />
-          <Image
-            className="absolute top-6 left-6"
-            src="/jata-negara.png"
-            width={50}
-            height={50}
-            alt="Jata Negara"
-          />
-          {typeof fileUpload !== 'string' && fileUpload.url && (
-            <Link
-              ref={link.ref}
-              className="absolute flex flex-col gap-6 inset-x-6 bottom-6"
-              href={fileUpload.url}
-              target="_blank"
-            >
-              <p className="text-body-xl font-semibold line-clamp-4 group-hover:text-txt-primary">
-                {name}
-              </p>
-              <Button
-                variant="outline"
-                className="group-hover:bg-bg-primary-600 group-hover:border group-hover:border-otl-primary-600 group-hover:text-white"
-              >
-                Lihat
-                <ArrowOutgoingIcon />
-              </Button>
-            </Link>
+    <Dialog>
+      <DialogTrigger asChild>
+        <article
+          className={cn(
+            'group flex flex-col gap-6 p-4.5 border rounded-lg overflow-hidden',
+            'hover:cursor-pointer hover:border-otl-primary-300 hover:ring ring-fr-primary',
+            className,
           )}
-        </>
-      ) : (
-        <></>
-      )}
-    </article>
+        >
+          <div className="relative w-full grow space-y-4">
+            {image && typeof image !== 'string' && (
+              <Media
+                htmlElement={null}
+                resource={image}
+                imgClassName="border rounded-md object-cover h-[200px]"
+              />
+            )}
+            <div className="space-y-3">
+              {name && (
+                <p className="font-semibold line-clamp-2 group-hover:text-txt-primary">{name}</p>
+              )}
+              {description && (
+                <p className="text-sm text-txt-black-500 line-clamp-2">{description}</p>
+              )}
+            </div>
+          </div>
+          <Button
+            variant="primary-outline"
+            className="group-hover:bg-bg-primary-600 group-hover:border group-hover:border-otl-primary-600 group-hover:text-white"
+          >
+            Info Lanjut
+            <ArrowOutgoingIcon />
+          </Button>
+        </article>
+      </DialogTrigger>
+      <DialogBody className="lg:min-w-[800px]">
+        <DialogHeader border>{name && <DialogTitle>{name}</DialogTitle>}</DialogHeader>
+        <DialogContent className="space-y-8">
+          {image && typeof image !== 'string' && (
+            <Media
+              htmlElement={null}
+              resource={image}
+              imgClassName="border rounded-md object-cover h-[350px]"
+            />
+          )}
+          {description && <DialogDescription>{description}</DialogDescription>}
+        </DialogContent>
+        <DialogFooter>
+          <DialogClose>
+            {fileUpload && typeof fileUpload !== 'string' && fileUpload.url && (
+              <Link href={fileUpload.url} target="_blank">
+                <Button variant="primary-fill">
+                  <DownloadIcon />
+                  Muat Turun Dokumen
+                </Button>
+              </Link>
+            )}
+          </DialogClose>
+        </DialogFooter>
+      </DialogBody>
+    </Dialog>
   )
 }
