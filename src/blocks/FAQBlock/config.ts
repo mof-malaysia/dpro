d1bd38d7b52176604be6aeb1e3290409c4524bb9
Len E-Herng <leneherng@gmail.com>
2025-08-20 09:33:45 +0800

    feat: helpdesk

--- src/blocks/FAQBlock/config.ts
+++ src/blocks/FAQBlock/config.ts
@@ -1,30 +1,30 @@
 import type { Block, Field } from 'payload'
-
 import {
   FixedToolbarFeature,
-  HeadingFeature,
   InlineToolbarFeature,
   lexicalEditor,
 } from '@payloadcms/richtext-lexical'
+import { linkGroup } from '@/fields/linkGroup'
 
 const columnFields: Field[] = [
   {
-    label: false,
+    label: 'Soalan',
     type: 'collapsible', // required
     fields: [
       {
         name: 'title',
+        label: 'Tajuk',
         type: 'text',
       },
       {
         name: 'richText',
+        label: 'Jawapan',
         type: 'richText',
         editor: lexicalEditor({
           features: ({ rootFeatures }) => {
             return [...rootFeatures, FixedToolbarFeature(), InlineToolbarFeature()]
           },
         }),
-        label: false,
       },
     ],
   },
@@ -37,16 +37,67 @@ export const FAQBlock: Block = {
     {
       name: 'title',
       type: 'text',
-      defaultValue: 'Soalan Lazim',
       label: 'Tajuk',
     },
     {
-      name: 'columns',
-      type: 'array',
-      admin: {
-        initCollapsed: true,
-      },
-      fields: columnFields,
+      name: 'desc',
+      type: 'richText',
+      label: 'Keterangan',
+      editor: lexicalEditor({
+        features: ({ rootFeatures }) => {
+          return [...rootFeatures, FixedToolbarFeature(), InlineToolbarFeature()]
+        },
+      }),
+    },
+    {
+      name: 'helpdesk',
+      type: 'group',
+      label: 'Meja Bantuan',
+      fields: [
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+        },
+        {
+          name: 'title',
+          type: 'text',
+          label: 'Tajuk',
+        },
+        {
+          name: 'desc',
+          type: 'richText',
+          label: 'Keterangan',
+          editor: lexicalEditor({
+            features: ({ rootFeatures }) => {
+              return [...rootFeatures, FixedToolbarFeature(), InlineToolbarFeature()]
+            },
+          }),
+        },
+        linkGroup({
+          appearances: ['primary-fill', 'primary-outline', 'default-outline'],
+          overrides: {
+            maxRows: 2,
+          },
+        }),
+      ],
+    },
+    {
+      name: 'faq',
+      type: 'group',
+      label: 'Soalan Lazim',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          label: 'Tajuk',
+        },
+        {
+          name: 'columns',
+          type: 'array',
+          fields: columnFields,
+        },
+      ],
     },
   ],
 }
