f14d125fb5c171cedbb6aae1c13a363c11a8a855
Len E-Herng <leneherng@gmail.com>
2025-08-22 16:55:35 +0800

    fix: config url for allowed origins and remote patterns

--- next.config.js
+++ next.config.js
@@ -2,15 +2,22 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.PROJECT_PRODUCTION_URL
-  ? process.env.PROJECT_PRODUCTION_URL
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+  ? process.env.NEXT_PUBLIC_SERVER_URL
   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+
+const urls = [NEXT_PUBLIC_SERVER_URL]
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: urls,
+    },
+  },
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...urls.map((item) => {
         const url = new URL(item)
 
         return {
